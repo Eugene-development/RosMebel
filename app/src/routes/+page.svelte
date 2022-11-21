c8e07f105cc0842e85159b9f963ce2cb29cf4a31
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-21 22:08:51 +0300

    с

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -116,6 +116,230 @@
 	];
 </script>
 
+
+<!-- <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
+	
+	<h1
+		class="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl"
+	>
+		<span class="relative whitespace-nowrap text-blue-600">
+			<svg
+				aria-hidden="true"
+				viewBox="0 0 418 42"
+				class="absolute top-2/3 left-0 h-[0.58em] w-full fill-blue-300/70"
+				preserveAspectRatio="none"
+			>
+				<path
+					d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"
+				/>
+			</svg>
+			<span class="relative">Мебель на заказ</span> <br />
+		</span>
+		для вашего дома и офиса
+	</h1>
+	<p class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
+		Предлагаем корпусную мебель на заказ от проверенных производителей и поставщиков. Гарантии и
+		поддержка 24/7.
+	</p>
+	<div class="mt-10 flex justify-center gap-x-6">
+		<button class="bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600" href="/register">Постоянное сопровождение проекта</button>
+         <button
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          variant="outline"
+        >
+          <svg
+            aria-hidden="true"
+            class="h-3 w-3 flex-none fill-blue-600 group-active:fill-current"
+          >
+            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
+          </svg>
+          <span class="ml-3">Watch video</span>
+        </button>
+	</div>
+	<div class="mt-36 lg:mt-44">
+		<p class="font-display text-base text-slate-900">Используемые технологии</p>
+		<ul
+          role="list"
+          class="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
+        >
+          {[
+            [
+              { name: 'Yandex', logo: logoYandex },
+              { name: 'Svelte', logo: logoSvelte },
+              { name: 'React', logo: logoReact },
+            ],
+            [
+              
+              { name: 'Laravel', logo: logoLaravel },
+              { name: 'GraphQL', logo: logoGraphQL },
+              { name: 'Nodejs', logo: logoNode }
+            ],
+          ].map((group, groupIndex) => (
+            <li key={groupIndex}>
+              <ul
+                role="list"
+                class="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0"
+              >
+                {group.map((company) => (
+                  <li key={company.name} class="flex">
+                    <Image src={company.logo} alt={company.name} unoptimized />
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+	</div>
+</div> -->
+
+
+
+<!-- This example requires Tailwind CSS v3.0+ -->
+<div class="isolate bg-white">
+  <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+    <svg class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
+      <defs>
+        <linearGradient id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#9089FC"></stop>
+          <stop offset="1" stop-color="#FF80B5"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  </div>
+  <div class="px-6 pt-6 lg:px-8">
+    <div>
+      <nav class="flex h-9 items-center justify-between" aria-label="Global">
+        <div class="flex lg:min-w-0 lg:flex-1" aria-label="Global">
+          <a href="/" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img class="h-8" src="https://storage.yandexcloud.net/brand-logo/azbuka-komforta/logo/logo2.png" alt="">
+          </a>
+        </div>
+        <div class="flex lg:hidden">
+          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <span class="sr-only">Open main menu</span>
+            <!-- Heroicon name: outline/bars-3 -->
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+        </div>
+        <div class="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
+          <a href="/" class="font-semibold text-gray-900 hover:text-gray-900">Замер</a>
+
+          <a href="/" class="font-semibold text-gray-900 hover:text-gray-900">Доставка</a>
+
+          <a href="/" class="font-semibold text-gray-900 hover:text-gray-900">Установка</a>
+          <a href="/" class="font-semibold text-gray-900 hover:text-gray-900">Оплата</a>
+
+          <a href="/" class="font-semibold text-gray-900 hover:text-gray-900">Гарантии</a>
+        </div>
+        <div class="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
+          <a href="/" class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20">Запись на замер</a>
+        </div>
+      </nav>
+      <!-- Mobile menu, show/hide based on menu open state. -->
+      <div role="dialog" aria-modal="true">
+        <div focus="true" class="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
+          <div class="flex h-9 items-center justify-between">
+            <div class="flex">
+              <a href="/" class="-m-1.5 p-1.5">
+                <span class="sr-only">Your Company</span>
+                <img class="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600" alt="">
+              </a>
+            </div>
+            <div class="flex">
+              <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                <span class="sr-only">Close menu</span>
+                <!-- Heroicon name: outline/x-mark -->
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div class="mt-6 flow-root">
+            <div class="-my-6 divide-y divide-gray-500/10">
+              <div class="space-y-2 py-6">
+                <a href="/" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Product</a>
+
+                <a href="/" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Features</a>
+
+                <a href="/" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Marketplace</a>
+
+                <a href="/" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Company</a>
+              </div>
+              <div class="py-6">
+                <a href="/" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10">Log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <main>
+    <div class="relative px-6 lg:px-8">
+      <div class="mx-auto max-w-7xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+        <div>
+          <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div class="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              <span class="text-gray-600">
+                Посетите наш мебельный блог <a href="/" class="font-semibold text-blue-600"><span class="absolute inset-0" aria-hidden="true"></span>Почитать <span aria-hidden="true">&rarr;</span></a>
+              </span>
+            </div>
+          </div>
+          <div>
+	<h1
+		class="mx-auto max-w-7xl text-center font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl"
+	>
+		<span class="relative whitespace-nowrap text-blue-600">
+			<svg
+				aria-hidden="true"
+				viewBox="0 0 418 42"
+				class="absolute top-2/3 left-0 h-[0.58em] w-full fill-blue-300/70"
+				preserveAspectRatio="none"
+			>
+				<path
+					d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"
+				/>
+			</svg>
+			<span class="relative">Мебель на заказ</span> <br />
+		</span>
+		для вашего дома и офиса
+	</h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+            <div class="mt-8 flex gap-x-4 sm:justify-center">
+              <a href="/" class="inline-block rounded-lg bg-red-900 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-red-800 hover:bg-blue-700 hover:ring-blue-700">
+                Получить скидку
+                <span class="text-red-100" aria-hidden="true">&rarr;</span>
+              </a>
+              <a href="/" class="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                Посмотреть каталог
+                <span class="text-gray-400" aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+            <svg class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
+              <defs>
+                <linearGradient id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#9089FC"></stop>
+                  <stop offset="1" stop-color="#FF80B5"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</div>
+
+
+
 <section
 	id="testimonials"
 	aria-label="What our customers are saying"
