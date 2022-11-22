dfacd4d61ab6a40a052dfa7a2f22c13afbe8702b
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-22 16:39:07 +0300

    с

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -299,32 +299,32 @@
             </div>
           </div>
           <div>
-	<h1
-		class="mx-auto max-w-7xl text-center font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl"
-	>
-		<span class="relative whitespace-nowrap text-blue-600">
-			<svg
-				aria-hidden="true"
-				viewBox="0 0 418 42"
-				class="absolute top-2/3 left-0 h-[0.58em] w-full fill-blue-300/70"
-				preserveAspectRatio="none"
-			>
-				<path
-					d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"
-				/>
-			</svg>
-			<span class="relative">Мебель на заказ</span> <br />
-		</span>
-		для вашего дома и офиса
-	</h1>
-            <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+		<h1
+			class="mx-auto max-w-7xl text-center font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl"
+		>
+			<span class="relative whitespace-nowrap text-blue-600">
+				<svg
+					aria-hidden="true"
+					viewBox="0 0 418 42"
+					class="absolute top-2/3 left-0 h-[0.58em] w-full fill-blue-300/70"
+					preserveAspectRatio="none"
+				>
+					<path
+						d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"
+					/>
+				</svg>
+				<span class="relative">Мебель на заказ</span> <br />
+			</span>
+			для вашего дома и офиса
+		</h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">Качественная и недорогая корпусная мебель на заказ с услугами доставки и установки.</p>
             <div class="mt-8 flex gap-x-4 sm:justify-center">
               <a href="/" class="inline-block rounded-lg bg-gradient-to-tr from-red-600 via-red-800 to-blue-600 hover:bg-gradient-to-bl px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1">
-                Получить скидку
+                Посмотреть каталог
                 <span class="text-red-100" aria-hidden="true">&rarr;</span>
               </a>
               <a href="/" class="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Посмотреть каталог
+                Заказать сборку
                 <span class="text-gray-400" aria-hidden="true">&rarr;</span>
               </a>
             </div>
