d3579f576e052d916114aaa490ecd836e8c56366
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-24 18:15:19 +0300

    c

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,15 +1,13 @@
 <script>
 	import '../app.css';
 	import Banner from '$lib/components/banner/index.svelte';
-	// import Header from '$lib/components/header/index.svelte';
+	import Header from '$lib/components/header/index.svelte';
 	import Footer from '$lib/components/footer/index.svelte';
 	export let data;
-
 </script>
 
 <Banner />
-
+<Header />
 <slot />
-
 <Footer {...data.contentFooter} />
 
