7a5c633a969ac848d38ccf68c9ced0102639a20c
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-14 14:05:39 +0300

    c

--- app/src/lib/components/forms/measurement/index.svelte
+++ app/src/lib/components/forms/measurement/index.svelte
@@ -192,6 +192,7 @@
 							</div>
 							<div class="flex flex-shrink-0 justify-end px-4 py-4">
 								<button
+                                    on:click={() => visibleFormMeasurement.update(invert)}
 									type="button"
 									class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
 									>Отменить</button
