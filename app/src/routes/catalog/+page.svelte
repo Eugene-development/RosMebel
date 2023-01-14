8ec0c8998d53fb7005ece502897bfc800e636779
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-14 20:21:47 +0300

    с

--- app/src/routes/catalog/+page.svelte
+++ app/src/routes/catalog/+page.svelte
@@ -28,16 +28,16 @@
         <ul class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:gap-x-8">
           <li>
             <a href="/shkafy">
-                            <div class="space-y-4">
+                            <div class="transition ease-in-out hover:scale-105 space-y-4 ">
               <div class="aspect-w-3 aspect-h-2">
-                <img class="rounded-lg object-cover shadow-lg" src="https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
+                <img class="rounded-lg object-cover shadow-lg" src="https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/1.jpg" alt="шкаф">
               </div>
               <div class="space-y-1 text-2xl font-medium leading-6">
                 <h3>Шкафы</h3>
                 <!-- <p class="text-indigo-600">Senior Front-end Developer</p> -->
               </div>
               <div class="text-lg">
-                <p class="text-gray-500">Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.</p>
+                <p class="text-gray-500">Посмотрите шкафы различного наполнения и цветового решения в нашем каталоге. Работаем по индивидуальным заказам.</p>
               </div>
             </div>
 
@@ -45,16 +45,16 @@
           </li>
           <li>
             <a href="/kuhni">
-            <div class="space-y-4">
+            <div class="transition ease-in-out hover:scale-105 space-y-4">
               <div class="aspect-w-3 aspect-h-2">
-                <img class="rounded-lg object-cover shadow-lg" src="https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
+                <img class="rounded-lg object-cover shadow-lg" src="https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV4/kuhni/elina.jpeg" alt="кухонный гарнитур">
               </div>
               <div class="space-y-1 text-2xl font-medium leading-6">
                 <h3>Кухонные гарнитуры</h3>
                 <!-- <p class="text-indigo-600">Senior Front-end Developer</p> -->
               </div>
               <div class="text-lg">
-                <p class="text-gray-500">Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.</p>
+                <p class="text-gray-500">Любой кухонные гарнитуры, которые вы закажите из каталога может быть выполнен в любой комплектации и любого размера.</p>
               </div>
             </div>
 
